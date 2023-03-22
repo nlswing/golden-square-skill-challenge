88aed4be6299ad719b06941e65f9ce05164b788e
Nathan Wing <wing_nathan@hotmail.com>
2023-03-22 11:17:12 +0000

    "Diary Entry Check Completed"

---
+++ 05_test_drive_a_class/spec/diary_entry_spec.rb
@@ -0,0 +1,87 @@
+require 'diary_entry'
+
+RSpec.describe DiaryEntry do
+  it "constructs" do
+    diary_entry = DiaryEntry.new("my_title", "my_contents")
+    expect(diary_entry.title).to eq "my_title"
+    expect(diary_entry.contents).to eq "my_contents"
+  end
+
+  describe "#count_words" do
+    it "returns the number of words in the contents" do
+      diary_entry = DiaryEntry.new("my_title", "some contents here")
+      expect(diary_entry.count_words).to eq 3
+    end
+    
+    it "returns zero when contents is empty" do
+      diary_entry = DiaryEntry.new("my_title", "")
+      expect(diary_entry.count_words).to eq 0
+    end
+  end
+
+  describe "#reading_time" do
+    context "given a wpm of some sensible number" do
+      it "return the ceiling of number of minutes it takes to read the contents" do
+        diary_entry = DiaryEntry.new("my_title", "one " * 500)
+        expect(diary_entry.reading_time(200)).to eq 3
+      end
+    end
+
+    context "given a wpm of 0" do
+      it "fails" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        expect { diary_entry.reading_time(0) }.to raise_error "Reading speed must be above zero."
+      end
+    end
+  end
+
+  describe "#reading_chunk" do
+    context "with a contents readable within the given minutes" do
+      it "returns the full contents" do
+      diary_entry = DiaryEntry.new("my_title", "one two three")
+      chunk = diary_entry.reading_chunk(200, 1)
+      expect(chunk).to eq "one two three"
+      end
+    end
+
+    context "with a contents unreadable in the given minutes" do
+      it "returns a readable chunk" do
+      diary_entry = DiaryEntry.new("my_title", "one two three")
+      chunk = diary_entry.reading_chunk(2, 1)
+      expect(chunk).to eq "one two"
+      end
+
+      context "given a wpm of 0" do
+        it "fails" do
+          diary_entry = DiaryEntry.new("my_title", "one two three")
+          expect { diary_entry.reading_chunk(0, 5) }.to raise_error "Reading speed must be above zero."
+        end
+      end
+
+      it "returns the nuext chunk, next time we are asked" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "three"
+      end
+
+      it "restarts after reading the whole contents" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        diary_entry.reading_chunk(2, 1)
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "one two"
+      end
+
+      it "restarts if it finishes exactly at the end" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        diary_entry.reading_chunk(1, 1)
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "one two"
+      end
+
+    end  
+  end
+end
+
